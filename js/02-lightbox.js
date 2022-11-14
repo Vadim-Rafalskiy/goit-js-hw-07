b03ac1d35162f123b3172425ba56768d7f782c10
Vadim-Rafalskiy <vadim.rafalskiy.zaknafer@gmail.com>
2022-11-14 14:02:20 +0200

    exercise #2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const ref = {
+  gallery: document.querySelector('.gallery'),
+};
+
+const render = arg => {
+  //   ref.gallery.innerHTML = '';
+  ref.gallery.insertAdjacentHTML('beforeend', arg);
+};
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}"><img class="gallery__image" alt="${description}"  src="${preview}"></a>`
+  )
+  .join('');
+
+render(markup);
+
+const handleImage = e => {
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  e.preventDefault();
+  //    gallery;
+  const gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+  gallery.on('shown.simpleLightbox');
+};
+
+ref.gallery.addEventListener('click', handleImage);
